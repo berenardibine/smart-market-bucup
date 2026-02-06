b7dc13c10bf919b0c870d6b796981ac79440be0d
Manishimwe Berenard <manishimweberenard@gmail.com>
2026-02-06 22:34:13 +0200

    Fix typo in setShowLocationModel variable

--- src/pages/AssetPage.tsx
+++ src/pages/AssetPage.tsx
@@ -23,7 +23,7 @@ const AssetPage = () => {
   
     const { 
     level, setLevel,
-    showLocationModal, 
+    showLocationModal, setShowLocationModel
     saveUserLocation, getLocationLabel 
   } = useUserLocation(); 
   
